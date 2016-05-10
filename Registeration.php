087f3abb137a394ab2a9566f9205479ae58902f6
Samad <samad_1309e@aptechgdn.com>
2016-05-11 01:46:00 +0500

    Registartion function is created

---
+++ Registeration.php
@@ -0,0 +1,17 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Paractice Website</title>
+</head>
+<body>
+ <h1>Registration page</h1>
+ <a href="index.php">Click here to Go Back</a>
+ <form action="Registeration.php" method="Post">
+ Enter Username : <input type="text" name="Username" required="required" /><br/>
+ Enter Password : <input type="password" name="password" required="required" /><br/>
+ <input type="submit" value="Register" />
+ 	
+ </form>
+
+</body>
+</html>
