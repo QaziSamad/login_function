aa764e1d43f415168010b46e784a08ad08cb279b
Samad <samad_1309e@aptechgdn.com>
2016-05-11 01:51:46 +0500

    form is created in login.php

--- login.php
+++ login.php
@@ -1,1 +1,15 @@
-<html
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>Paractice Website</title>
+</head>
+<body>
+ <h1>Login Page</h1>
+ <a href="index.php">Click here to go back</a>
+ <form action="login.php" method="Post">
+ 	Enter username : <input type="text" name="username" required="required" /><br/>
+ 	Enter password : <input type="password" name="password" required="required" /><br/>
+ 	<input type="Submit" value="Login">
+ </form>
+</body>
+</html>
