05ede1ee60e1797c7dcd6d265cae1fc5b83bc873
Samad <samad_1309e@aptechgdn.com>
2016-05-11 12:01:28 +0500

    simple login and registration created with db conectivity

--- Registeration.php
+++ Registeration.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Paractice Website</title>
@@ -7,7 +7,7 @@
  <h1>Registration page</h1>
  <a href="index.php">Click here to Go Back</a>
  <form action="Registeration.php" method="Post">
- Enter Username : <input type="text" name="Username" required="required" /><br/>
+ Enter Username : <input type="text" name="username" required="required" /><br/>
  Enter Password : <input type="password" name="password" required="required" /><br/>
  <input type="submit" value="Register" />
  	
@@ -15,3 +15,34 @@
 
 </body>
 </html>
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+	$username = mysql_real_escape_string($_POST['username']);
+	$password = mysql_real_escape_string($_POST['password']);
+	$bool = true;
+
+	mysql_connect("localhost" , "root" , "") or die(mysql_error());
+	mysql_select_db("first_db") or die("can not connect ");
+	$query = mysql_query("select * from users");
+	while ($row = mysql_fetch_array($query)) 
+	{
+		$table_users = $row['username'];
+		if ($username == $table_users) {
+			$bool = false;
+			print '<script>alert("Username has been taken !");</script>';
+			print '<script>window.location.assign("Registeration.php");</script>';
+	}
+	
+}
+
+
+if($bool)
+{
+	mysql_query("INSERT INTO users (username , password) VALUES ('$username' , '$password')");
+	print '<script>alert("Successfully registered !");</script>';
+	print '<script>window.location.assign("Registeration.php");</script>';  
+}
+}
+
+?>
--- login.php
+++ login.php
@@ -1,4 +1,4 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Paractice Website</title>
@@ -6,7 +6,7 @@
 <body>
  <h1>Login Page</h1>
  <a href="index.php">Click here to go back</a>
- <form action="login.php" method="Post">
+ <form action="check_login.php" method="Post">
  	Enter username : <input type="text" name="username" required="required" /><br/>
  	Enter password : <input type="password" name="password" required="required" /><br/>
  	<input type="Submit" value="Login">
